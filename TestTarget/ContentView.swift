c461e1fe8f432385efbd8dd7b04c32facb95de83
Rubens Gondek <rubens.gondek@gmail.com>
2020-08-16 23:38:28 -0300

    In Progress

---
+++ TestTarget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestTarget
+//
+//  Created by Rubens Gondek on 11/21/19.
+//  Copyright © 2019 Gondek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
